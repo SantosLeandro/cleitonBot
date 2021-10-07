5775e75c50d9e09b2343ed4fd9652b448df870ca
SantosLeandro <31940650+SantosLeandro@users.noreply.github.com>
2021-10-07 14:48:23 -0300

    ajuste

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $message = $update["message"]["text"];
 $id = $update["message"]["message_id"];
 
 $sale = date('25/06/2020');
- if(rand(1, 5)==2){
+ if(rand(1, 8)==3){
 	 $name = $update["message"]["from"]["first_name"];
      $msg = array("cheguei man",$name." é massa","chance","qual a dúvida","ate no rabo","ihh rapaz",
                   "cala boca demonho","satanás caluniador","Sai daquiiii demonho");
