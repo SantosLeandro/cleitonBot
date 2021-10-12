1a071d743fb3e8309b7feb2ef224c5bafb3160bf
SantosLeandro <31940650+SantosLeandro@users.noreply.github.com>
2021-10-12 12:32:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $id = $update["message"]["message_id"];
 
 $sale = date('25/06/2020');
 
-if($message["dice"]){
+if($message["dice"]["emoji"]!=''){
 	sendMessage($chatId, "emoji ".$message["dice"]["emoji"]." valor ".$message["value"]);	
 }
 
