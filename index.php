4f8e5a329c8b33c75bddc08c57da5cb770ee6bac
SantosLeandro <31940650+SantosLeandro@users.noreply.github.com>
2022-01-06 22:04:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -60,6 +60,12 @@ if(checkCommand($message, "/entende")){
   sendMessage($chatId, "Olá como vai entende");
 }
 
+if($message == "." && $update["message"]["reply_to_message"]["text"] != '')){
+  $name = $update["message"]["reply_to_message"]["from"]["first_name"];	 
+  $msg = $update["message"]["reply_to_message"]["text"];
+  file_get_contents("https://digoboratv.000webhostapp.com/api/addImportantes.php?nome=".urlencode($name)."&msg=".urlencode($msg));
+}
+
 if(checkCommand($message, "/registrar")){
   $name = $update["message"]["reply_to_message"]["from"]["first_name"];	 
   $msg = $update["message"]["reply_to_message"]["text"];
